060c9269d7761e82fc638063e90533d20fbe9fe8
ykawaguchi <ykawaguchi.1227@gmail.com>
2021-08-25 12:32:36 +0900

    rect追加

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ function App() {
   return (
     <Stage width={500} height={500}>
       <Layer>
-        <Rect fill='red' width={300} height={200} />
+        {/* 枠です。見やすくするためにあります。 */}
+        <Rect stroke='black' strokeWidth={1} x={0} y={0} width={500} height={500} />
+        <Rect fill='red' x={100} y={100} width={300} height={200} />
       </Layer>
     </Stage>
   );
