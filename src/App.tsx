c713fdbfd9f8e1211b28bd4c3715e0475b7497c6
ykawaguchi <ykawaguchi.1227@gmail.com>
2021-08-25 12:02:26 +0900

    react-konva追加 最低限のrectを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Stage, Layer, Rect } from 'react-konva';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Stage width={500} height={500}>
+      <Layer>
+        <Rect fill='red' width={300} height={200} />
+      </Layer>
+    </Stage>
   );
 }
 
