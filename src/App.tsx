ae9c563b548a15081a9a0ae117a5d6553d87e922
ykawaguchi <ykawaguchi.1227@gmail.com>
2021-08-25 13:21:22 +0900

    テキスト追加、画像追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,11 @@
 import React from 'react';
-import { Stage, Layer, Rect, Circle } from 'react-konva';
+import { Stage, Layer, Rect, Circle, Text, Image } from 'react-konva';
+import logo from './logo.svg';
 
 
 function App() {
-  return (
-    <div>
-      <Stage width={500} height={500}>
-        <Layer>
-          {/* 枠です。見やすくするためにあります。 */}
-          <Rect stroke='black' strokeWidth={1} x={0} y={0} width={500} height={500} />
-          <Rect fill='red' x={100} y={100} width={300} height={200} />
-        </Layer>
-      </Stage>
-    </div>
-  );
-}
-
-/*
-function App() {
+  let img = new window.Image();
+  img.src = logo;
   return (
     <div>
       <Stage width={500} height={500}>
@@ -26,14 +14,29 @@ function App() {
           <Rect fill='red' x={100} y={100} width={300} height={200} />
         </Layer >
       </Stage >
-  <Stage width={500} height={500}>
-    <Layer>
-      <Rect stroke='black' strokeWidth={1} x={0} y={0} width={500} height={500} />
-      <Circle fill='red' x={100} y={100} radius={150} />
-    </Layer>
-  </Stage>
+
+      <Stage width={500} height={400}>
+        <Layer>
+          <Rect stroke='black' strokeWidth={1} x={0} y={0} width={500} height={400} />
+          <Circle fill='red' x={100} y={100} radius={150} />
+        </Layer>
+      </Stage>
+
+      <Stage width={500} height={100}>
+        <Layer>
+          <Rect stroke='black' strokeWidth={1} x={0} y={0} width={500} height={100} />
+          <Text text='aaaあああ' width={100} height={100} />
+        </Layer>
+      </Stage>
+
+      <Stage width={500} height={200}>
+        <Layer>
+          <Rect stroke='black' strokeWidth={1} x={0} y={0} width={500} height={200} />
+          <Image image={img} width={img.width} height={img.height} />
+        </Layer >
+      </Stage >
     </div >
   );
-}*/
+}
 
 export default App;
